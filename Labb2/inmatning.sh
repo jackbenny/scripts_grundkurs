215bd9aa9a1830a6253f1a36498af46cb1675475
Jack-Benny Persson <jack-benny@cyberinfo.se>
2013-12-10 13:15:49 +0100

    Ändrat Namn till en declare

--- Labb2/inmatning.sh
+++ Labb2/inmatning.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Definiera Name
-Name="Hemligt"
+declare Namn
 
 read -t 3 -p "Vad heter du? " Name
-Name=${Name:="Hemligt"}  # Definiera Name till Hemligt om ingen input
+Name=${Name:="Secret"}  # Definiera Name till Hemligt om ingen input
 printf "Du heter: $Name\n\n" 
 
 exit 0
